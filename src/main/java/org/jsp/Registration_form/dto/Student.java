e9c8921a137a0c8ef2ae2615a71645e981736710
Poornima.G <poornig183@gmail.com>
2024-12-13 11:57:57 +0530

    razorpay

--- src/main/java/org/jsp/Registration_form/dto/Student.java
+++ src/main/java/org/jsp/Registration_form/dto/Student.java
@@ -10,9 +10,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
-@Data
+
+
 @Entity
 public class Student {
     @Id
@@ -28,4 +28,35 @@ public class Student {
     @Transient
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
     private String confirmPassword;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+    
 }
--- src/main/java/org/jsp/Registration_form/helper/MailHelper.java
+++ src/main/java/org/jsp/Registration_form/helper/MailHelper.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import jakarta.mail.internet.MimeMessage;
+
 
 @Component
 public class MailHelper {
@@ -14,18 +17,28 @@ public class MailHelper {
     @Autowired
     JavaMailSender sender;
 
+      @Autowired
+    TemplateEngine engine;
+
     public void sendEmail(Student student) {
         MimeMessage message=sender.createMimeMessage();
         MimeMessageHelper helper=new MimeMessageHelper(message);
         try{
-        helper.setFrom("poornig183@gmail.com","Head Master");
-        helper.setText("Admission Successfull!!! Thank you for Creaing Account");
-        helper.setSubject("Registration Successful");
-        helper.setTo(student.getEmail());
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-        sender.send(message);
+            helper.setFrom("poornig183@gmail.com","Head Master");
+           
+            Context context=new Context();
+            context.setVariable("student", student);
+    
+            String text=engine.process("email-template.html", context);
+           
+            helper.setText(text,true);
+            
+            helper.setSubject("Registration Successful");
+            helper.setTo(student.getEmail());
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+            sender.send(message);
     }
     
 }
